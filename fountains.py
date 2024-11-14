8bc99ac2bf3e97099798f0c7a1a3120cbef11495
Samin <samin.mhr2004@gmail.com>
2024-11-14 16:34:35 +0330

    control the cycle of the fountains

--- fountains.py
+++ fountains.py
@@ -6,24 +6,28 @@ import threading
 # creating a lock so with that make sure only one tread can accsess printing
 print_lock = threading.Lock()
 
-# control the time of being on and off 
-def control_fountain(fountain_id, on_time, off_time):
+# control the time of being on and off and cycles
+def control_fountain(fountain_id):
+    on_time = random.randint(1, 5)
+    off_time = random.randint(1, 5)
+    cycles = random.randint(1, 3)
 
-    # fountain start in current time and print the giving format
-    with print_lock:
-        start_time = datetime.now().strftime("%H:%M:%S")
-        print(f"Fountain {fountain_id} started in : {start_time}")
+    for _ in range(cycles):
+        # fountain start in current time and print the giving format
+        with print_lock:
+            start_time = datetime.now().strftime("%H:%M:%S")
+            print(f"Fountain {fountain_id} started in : {start_time}")
 
-    #sleep the time as being on
-    time.sleep(on_time)
+        #sleep the time as being on
+        time.sleep(on_time)
 
-    # fountain end in current time and print the giving format
-    with print_lock:
-        end_time = datetime.now().strftime("%H:%M:%S")
-        print(f"Fountain {fountain_id} ended in : {end_time}")
-    
-    #sleep the time as being off
-    time.sleep(off_time)
+        # fountain end in current time and print the giving format
+        with print_lock:
+            end_time = datetime.now().strftime("%H:%M:%S")
+            print(f"Fountain {fountain_id} finished in : {end_time}")
+        
+        #sleep the time as being off
+        time.sleep(off_time)
 
 
 # control the fountains start and end sequentially
@@ -32,9 +36,9 @@ def sequential_fountains():
     
     # creat 3 treads as fountains and in each treads run the control_fountain fonction and give the fountain id, on time and off time (randomly) as args
     threads = [
-        threading.Thread(target=control_fountain, args=(1, random.randint(1, 5), random.randint(1, 5))),
-        threading.Thread(target=control_fountain, args=(2, random.randint(1, 5), random.randint(1, 5))),
-        threading.Thread(target=control_fountain, args=(3, random.randint(1, 5), random.randint(1, 5)))
+        threading.Thread(target=control_fountain, args=(1,)),
+        threading.Thread(target=control_fountain, args=(2,)),
+        threading.Thread(target=control_fountain, args=(3,))
     ]
 
     # fountain[n].start start the fountain and fountain[n].join() make sure that the first thread do the job and finish and then run the other line
@@ -42,7 +46,6 @@ def sequential_fountains():
         thread.start()
         thread.join()
     
-
 
 # control the fountains start and end two by two
 def pairs_fountains():
@@ -54,14 +57,11 @@ def pairs_fountains():
 
     # for each pair creat a same random time for on and off and creat a thread as fountains
     for pair in pairs:
-        on_time = random.randint(1, 5)
-        off_time = random.randint(1, 5)
-
         threads = []
 
         for fountain_id in pair:
 
-            thread = threading.Thread(target=control_fountain, args=(fountain_id, on_time, off_time))
+            thread = threading.Thread(target=control_fountain, args=(fountain_id,))
             threads.append(thread)
             thread.start()
     
@@ -74,13 +74,10 @@ def all_simultaneously():
 
     print("\n[ All fountains simultaneously ]")
 
-    on_time = random.randint(1, 5)
-    off_time = random.randint(1, 5)
-
     threads = [
-        threading.Thread(target=control_fountain, args=(1, on_time, off_time)),
-        threading.Thread(target=control_fountain, args=(2, on_time, off_time)),
-        threading.Thread(target=control_fountain, args=(3, on_time, off_time))
+        threading.Thread(target=control_fountain, args=(1,)),
+        threading.Thread(target=control_fountain, args=(2,)),
+        threading.Thread(target=control_fountain, args=(3,))
     ]
 
     for thread in threads:
